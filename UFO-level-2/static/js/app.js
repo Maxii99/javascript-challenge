69a7f8346e6f1cf893aa81c70884e5de4c745a2a
Maxwell Oteng <maxwelloteng@Maxwells-MacBook-Pro-2.local>
2020-10-01 00:58:33 -0700

    updated

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,10 +1,12 @@
 // from data.js
-var tableData = data;
+
 
 // YOUR CODE HERE!
+var tableData = data;
 
 // Console.log tableData
 console.log(tableData);
+
 
 // Reference the table
 var tbody=d3.select("tbody");
@@ -31,23 +33,44 @@ var button = d3.select("#filter-btn");
 button.on("click", function() {
     tbody.html("");
 
+// Remove existing table
+d3.select("tbody").html("");
+
 // Prevent page from refreshing
 d3.event.preventDefault();
 
-// Get the value property of input elements
-var inputValue = d3.select("#input").property("value");
- 
-    console.log(inputValue);
 
-// Filter Data for value property of input elements date, country, state, city and shape
-var filteredData = tableData.filter(sighting => sighting.datetime === inputValue ||
-                                                sighting.city === inputValue ||
-                                                sighting.state === inputValue ||
-                                                sighting.country === inputValue ||
-                                                sighting.shape === inputValue);
+// // Get the value property of input elements
+// var inputValue = d3.select("#input").property("value");
     
+var dateTime = d3.select("#datetime").property("value");
+var selectedCountry = d3.select("#country").property("value").toLowerCase();
+var selectedState = d3.select("#state").property("value").toLowerCase();
+var selectedCity = d3.select("#city").property("value").toLowerCase();
+var selectedShape = d3.select("#shape").property("value").toLowerCase();
 
-    console.log(filteredData);
+// Initialize tableData as filteredData
+filteredData = tableData;
+
+// Set if conditions for filter
+
+if (dateTime){
+    filteredData=filteredData.filter(sighting => sighting.datetime===dateTime);
+}
+if (selectedCountry){
+    filteredData=filteredData.filter(sighting => sighting.country===selectedCountry);
+}
+if (selectedState){
+    filteredData=filteredData.filter(sighting => sighting.state===selectedState);
+}
+if (selectedCity){
+    filteredData=filteredData.filter(sighting => sighting.city===selectedCity);
+}
+if (selectedShape){
+    filteredData=filteredData.filter(sighting => sighting.shape===selectedShape);
+}
+
+    // console.log(filteredData);
 
     filteredData.forEach(function(selections) {
         console.log(selections);
